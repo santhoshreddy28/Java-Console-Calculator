3a4ffeb2ccacec18bfb6bfcc2589b49691dbddab
santhosh reddy <151549016+santhoshreddy28@users.noreply.github.com>
2026-06-06 17:09:16 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    // Addition
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Cannot divide by zero!");
+            return 0;
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n===== JAVA CONSOLE CALCULATOR =====");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            if (choice >= 1 && choice <= 4) {
+
+                System.out.print("Enter first number: ");
+                double num1 = sc.nextDouble();
+
+                System.out.print("Enter second number: ");
+                double num2 = sc.nextDouble();
+
+                switch (choice) {
+
+                    case 1:
+                        System.out.println("Result = " + add(num1, num2));
+                        break;
+
+                    case 2:
+                        System.out.println("Result = " + subtract(num1, num2));
+                        break;
+
+                    case 3:
+                        System.out.println("Result = " + multiply(num1, num2));
+                        break;
+
+                    case 4:
+                        System.out.println("Result = " + divide(num1, num2));
+                        break;
+
+                    default:
+                        System.out.println("Invalid Choice!");
+                }
+            }
+
+        } while (choice != 5);
+
+        System.out.println("Calculator Closed.");
+        sc.close();
+    }
+}
